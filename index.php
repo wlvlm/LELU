fbe46388f02f8c41216f3345e3c77dc774a265b3
William Vuillaume <williampgm77@gmail.com>
2023-10-14 05:38:03 +0200

    ajout du favicon et du head

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/img/LELU_favicon.png" type="image/x-icon">
+    <title>LELU</title>
+</head>
+<body>
+    <h1>TEST</h1>
+</body>
+</html>

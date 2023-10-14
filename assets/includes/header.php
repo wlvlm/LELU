6641452d53df35916852f56ae5e6493627470668
William Vuillaume <williampgm77@gmail.com>
2023-10-14 05:50:34 +0200

    Init css et php

---
+++ assets/includes/header.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/img/LELU_favicon.png" type="image/x-icon">
+    <title>LELU</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="assets/img/LELU_logo.svg" alt="Logo de LELU" class="logo">
+            <ul class="nav">
+                <li><a href="index.php">Accueil</a></li>    
+                <li><a href="catalog.php">Parcourir</a></li>
+                <li><a href="community.php">Communauté</a></li>
+                <?php if(isset($_SESSION['account_id'])){ ?> 
+                <li><a href="acocunt.php">Mon compte</a></li>
+                <?php } else { ?>
+                <li><a href="login.php">Se connecter</a></li>
+                <?php } ?>
+            </ul>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="shortcut icon" href="assets/img/LELU_favicon.png" type="image/x-icon">
-    <title>LELU</title>
-</head>
-<body>
-    <h1>TEST</h1>
-</body>
-</html>
+<?php 
+include_once('assets/includes/header.php')
+?>
+
+<main>
+    <h1>index</h1>
+</main>
